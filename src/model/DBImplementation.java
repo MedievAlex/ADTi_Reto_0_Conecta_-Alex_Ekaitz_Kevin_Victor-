38a4522e4055175e34d2267f41afe70f0a83497b
MedievAlex <alexirazola@gmail.com>
2025-09-18 13:54:27 +0200

    Creating missing files

---
+++ src/model/DBImplementation.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.sql.*;
+import java.util.*;
+
+public class DBImplementation implements ModelDAO {
+	// Prepare statement variables
+	private Connection con;
+	private PreparedStatement stmt;
+
+	// Prepare SQL Connection variables (with a supressed warning, as it's kind of annoying to see)
+	private ResourceBundle configFile;
+	@SuppressWarnings("unused")
+	private String driverBD;
+	private String urlBD;
+	private String userBD;
+	private String passwordBD;
+
+}
