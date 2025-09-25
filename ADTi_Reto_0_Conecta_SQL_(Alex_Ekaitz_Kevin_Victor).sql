ec6fb22d96b98f1a4ef08cc7c8eab62bda3b19b2
MedievAlex <alexirazola@gmail.com>
2025-09-25 12:33:26 +0200

    Created README and added info to the DB

--- ADTi_Reto_0_Conecta_SQL_(Alex_Ekaitz_Kevin_Victor).sql
+++ ADTi_Reto_0_Conecta_SQL_(Alex_Ekaitz_Kevin_Victor).sql
@@ -35,17 +35,17 @@ FOREIGN KEY (E_ID) REFERENCES ExamStatement (ID) ON UPDATE CASCADE ON DELETE CAS
 );
 
 INSERT INTO TeachingUnit (ACRONIM, TITLE, EVALUATION, DESCRIPTION) VALUES
-("DIN", "Desarollo de Interfaces", "Primera", "Descripcion de ejemplo 3"),
-("ADTi", "Acceso a Datos Inglés", "Tercera", "Descripcion de ejemplo 4");
+("DIN", "Desarollo de Interfaces", "Primera", "Implanta sistemas y aplicaciones informáticas sobre entornos específicos."),
+("ADTi", "Acceso a Datos Inglés", "Tercera", "Desarrolla aplicaciones y componentes de acceso a datos, gestionando su persistencia y garantizando la seguridad y calidad de los mismos.");
 
 INSERT INTO ExamStatement (DESCRIPTION, STATEMENT_LEVEL, AVAIABLE, RUTA) VALUES
-("Descripcion de ejemplo 1", "ALTO", true, "Por la derecha"),
-("Descripcion de ejemplo 2", "MEDIO", false, "A la izquierda");
+("Genera un programa con interfaz para una aplicación de compraventa utilizando una base de datos.", "ALTO", true, "Por la derecha"),
+("Crea un esquema del funcionamiento de una aplicacion generica.", "BAJO", false, "A la izquierda");
 
 INSERT INTO StatementUnit VALUES
 ("DIN", 1),
 ("ADTi", 1);
 
 INSERT INTO ExamSession VALUES
-("Primera Convocatoria", "Descripcion de ejemplo 5", '2025-01-04', "Mantenimiento", 1),
-("Segunda Convocatoria", "Descripcion de ejemplo 6", '2025-02-24', "Electronica", 1);
+("Primera Convocatoria", "Examen para la clase 208", '2025-01-04', "2DAMi", 1),
+("Segunda Convocatoria", "Examend e recuperacion tanto para 1º como 2º DAMi", '2025-02-24', "2DAMi", 1);
