e9b1dbb650dab9544493b7ebc1ea7e5ba10c2d61
MedievAlex <alexirazola@gmail.com>
2025-09-30 11:31:06 +0200

    Fixes

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,6 @@
 package main;
 
 import controller.Controller;
-import model.ExamStatement;
-import model.TeachingUnit;
 import utilidades.Utilidades;
 
 /**
@@ -26,60 +24,6 @@ public class Main {
         System.out.println("[ 5 ] Consult in which EXAM SESSIONS a specific EXAM STATEMENT has been used");
         System.out.print("Choose: ");
         return Utilidades.leerInt(0, 5);
-    }
-
-    /**
-     * Create a EXAM SESSION (Convocatoria) by adding an existent STATEMENT
-     * (Enunciado).
-     *
-     * @param cont
-     */
-  
-    /*public static void createExamSession(Controller cont) {
-        ExamSession examSession = new ExamSession();
-        boolean exists = true;
-
-        System.out.print("Enter the CONVOCATORIA: ");
-        examSession.setConvocatoria(Utilidades.introducirCadena());
-        System.out.print("Enter the DESCRIPTION: ");
-        examSession.setDescription(Utilidades.introducirCadena());
-        System.out.print("Enter the DESCRIPTION: ");
-        examSession.setSession_date(Date.valueOf(Utilidades.leerFechaAMD()));
-        System.out.print("Enter the COURSE: ");
-        examSession.setCourse(Utilidades.introducirCadena());
-
-        System.out.println("[ AVAILABLE EXAM STATEMENTS ]");
-        cont.showAllExamStatements();
-
-        do {
-            System.out.print("Add the EXAM STATEMENTS's ID: ");
-            ExamStatement examStatement = new ExamStatement(Utilidades.leerInt());
-            exists = cont.verifyExamStatement(examStatement);
-            if (exists) {
-                System.out.print("[ ERROR ] Invalid ID");
-            }
-        } while (exists);
-        cont.newExamSession(examSession);
-    }*/
-
-    /**
-     * Consult the EXAM STATEMENT (Enunciado) by TEACHING UNIT
-     * (UnidadDIdactica).
-     *
-     * @param cont
-     */
-    public static void checkStatementByTeachingUnit(Controller cont) {
-        cont.consultStatementByTeachingUnit();
-    }
-
-    /**
-     * Consult in which EXAM SESSIONS (Convocatoria) a specific EXAM STATEMENT
-     * (Enunciado) has been used.
-     *
-     * @param cont
-     */
-    public static void checkSessionsByStatement(Controller cont) {
-        cont.consultSessionsByStatement();
     }
 
     /**
